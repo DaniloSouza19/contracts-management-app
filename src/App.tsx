8f1a51cc2aa3c4dfce51925016362f390b242388
Danilo Souza <danilo_souza19@outlook.com>
2022-03-19 02:01:36 -0300

    fix: add AppProvider after Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { StyledEngineProvider } from '@mui/material/styles';
+import { AppProvider } from './hooks';
 import { Router } from './Router';
 
 export function App() {
   return (
     <div className="App">
       <StyledEngineProvider injectFirst>
-        <Router />
+        <AppProvider>
+          <Router />
+        </AppProvider>
       </StyledEngineProvider>
     </div>
   );
