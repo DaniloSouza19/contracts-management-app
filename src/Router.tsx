d44602da98695bcdf504e57ab9c444b08db061b6
Danilo Souza <danilo_souza19@outlook.com>
2022-03-19 17:47:11 -0300

    feat: add requireAuth to make private route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,37 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+  Navigate,
+} from 'react-router-dom';
+import { useAuth } from './hooks/auth';
 import { Dashboard } from './pages/Dashboard';
 import { SignIn } from './pages/SignIn';
+
+function RequireAuth({ children }: { children: JSX.Element }) {
+  const { user } = useAuth();
+  const location = useLocation();
+
+  if (!user) {
+    return <Navigate to="/sign-in" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <Dashboard />
+            </RequireAuth>
+          }
+        />
         <Route path="/sign-in" element={<SignIn />} />
       </Routes>
     </BrowserRouter>
