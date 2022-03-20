7f258755f55faeff8bbebe8c7cef45d725d1b69a
Danilo Souza <danilo_souza19@outlook.com>
2022-03-20 19:43:20 -0300

    lint

--- src/utils/calculateValues.ts
+++ src/utils/calculateValues.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 export const calculateValueFromPercentage = (
   percentage: number,
-  totalValue: number,
+  totalValue: number
 ): number => {
   return totalValue * (percentage / 100);
 };
--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/prefer-default-export */
 import { parseISO, format } from 'date-fns';
 
 export const formatValueAsCurrency = (value: number): string => {
@@ -22,7 +21,7 @@ export const formatValueWithThousandsSeparator = (value: number): string => {
 
 export const formatStringDate = (
   date: string,
-  patternDateFns: string,
+  patternDateFns: string
 ): string => {
   const dateParsed = parseISO(date);
 
