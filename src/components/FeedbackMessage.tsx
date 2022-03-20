c80d8e2a66199cd26cee4fb79f85e9d38497890e
Danilo Souza <danilo_souza19@outlook.com>
2022-03-20 01:56:23 -0300

    remove unusal imports

--- src/components/FeedbackMessage.tsx
+++ src/components/FeedbackMessage.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
-import { makeStyles, Theme } from '@material-ui/core/styles';
 
 function Alert(props: AlertProps) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
-const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    width: '100%',
-    '& > * + *': {
-      marginTop: theme.spacing(2),
-    },
-  },
-}));
+
 interface FeedBackMessageProps {
   message: string;
   severity: 'success' | 'info' | 'error';
